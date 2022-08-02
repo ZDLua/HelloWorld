9edafbb9b4b5d8d46abfac074a0fd14d52a3c2d3
ZDLua <hj_jr@icloud.com>
2022-08-02 08:21:16 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, a new console app has been created!");
+        }
+    }
+}
